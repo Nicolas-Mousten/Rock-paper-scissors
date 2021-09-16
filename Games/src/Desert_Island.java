ff420bb822314b426b1d7fa3d662f70eb02b3bdd
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-09-16 10:27:10 +0200

    Finish with desert island

--- Games/src/Desert_Island.java
+++ Games/src/Desert_Island.java
@@ -2,33 +2,31 @@ import java.util.Scanner;
 
 public class Desert_Island {
     public static int[] newArray;
-    public static int getGreatest(int[]array,int len){
-        int greatestElement;
+    public static String getGreatest(int[]array,int len){
+        String greatestElement ="";
         int[] temporaryArray = new int[len];
-
 
         for (int i = 0; i < array.length; i++) {
             temporaryArray[i] = newArray[i];
         }
+
         for (int i = 0; i < temporaryArray.length; i++) {
             if(temporaryArray[i]>temporaryArray[0]){
                 temporaryArray[0] = temporaryArray[i];
             }
         }
+
         //get the index of the greatest number
-        int greatestInt = temporaryArray[0];
         for (int i = 0; i < newArray.length; i++) {
             if(newArray[i] == temporaryArray[0]){
                 //i is saved for later use
-
+                String patternNumber = ""+i;
+                greatestElement += patternNumber;
             }
         }
-
-        greatestElement=1;
         return greatestElement;
     }
     public static int[] newArray(int[] oldArray, int x, int answer){
-        //int[] newArray;
         //Creating the elements equal to the old array
         newArray = new int[x];
         //creating new array
@@ -41,10 +39,11 @@ public class Desert_Island {
                 newArray [i] = newArray[i]+1;
             }
         }
-        //Printing new array with changes
+
+/*      //Printing new array with changes
         for (int i = 0; i < x; i++) {
             System.out.print(newArray[i] + ",");
-        }
+        }*/
         System.out.println("\n");
         return newArray;
     }
@@ -55,7 +54,7 @@ public class Desert_Island {
         int[] answerArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         String[] options = {"Lover","Coffee machine","Netflix","Couch","Running boots","Guitar","Candy","Dog","Book","Beer"};
 
-
+        String grammer ="";
         boolean lock = true;
         while(lock == true) {
             for (int i = 0; i < options.length; i++) {
@@ -66,9 +65,18 @@ public class Desert_Island {
             scanner.nextLine();
 
             answerArray = newArray(answerArray, options.length, answer);
-            int greatestElement;
+            String greatestElement;
             greatestElement = getGreatest(answerArray,answerArray.length);
+            int indexGreatest = 0;
+            for (int j = 0; j < options.length; j++) {
+                System.out.print(newArray[j] + ",");
+            }
+            for (int i = 0; i < greatestElement.length(); i++) {
+                grammer = greatestElement.length()>=2 ? "s":"";
+                indexGreatest = Integer.parseInt(String.valueOf(greatestElement.charAt(i)));
 
+                System.out.println("\nMost popular choice"+grammer+" is: " + options[indexGreatest]);
+            }
             System.out.println("try again? else write = q");
             String quitLock = scanner.nextLine();
             if (quitLock.equalsIgnoreCase("q")){
